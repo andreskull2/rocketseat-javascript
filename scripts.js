0bf33bf6f7bf33963ff3926bf69350fbb13665f1
andreskull2 <andreskull1@gmail.com>
2025-01-27 22:01:46 -0300

    Comentários em JavaScript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+// O console.log exibe no console um texto.
 console.log("Andre")
+
+/*
+Cometário de múltiplas linhas.
+
+Comentários são trechos de texto que podem ser adicionados junto com o código.
+
+O navegador ignora o texto marcado como comentário.
+
+Porque utilizar comentários durante seus estudos:
+- Fornecer informações adicionais sobre o que o código faz;
+- Deixar lembretes de coisas a fazer;
+- Relembrar no futuro o que aquilo faz;
+- Etc...
+*/
